3867766c54ac7603c6917d5723e84256b51d6bfc
Zainab Conteh <alykhanu@gmail.com>
2025-03-27 19:09:11 +0000

    Settings for render updated 2

--- connect_salone/settings.py
+++ connect_salone/settings.py
@@ -115,12 +115,12 @@ DATABASES = {
 
 
 #ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="yourdomain.com").split(",")
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =['connect-salone.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['connect-salone.onrender.com']
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ['connect-salone.onrender.com']
 
 
 
